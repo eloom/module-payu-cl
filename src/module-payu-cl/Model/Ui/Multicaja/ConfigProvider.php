dde3ae3976cc350d65c715eb650d253171b47f58
dholi <comunidade@dholi.dev>
2021-10-17 16:40:26 -0300

    v 1.0.2

--- src/module-payu-cl/Model/Ui/Multicaja/ConfigProvider.php
+++ src/module-payu-cl/Model/Ui/Multicaja/ConfigProvider.php
@@ -3,10 +3,10 @@
 * 
 * PayU Chile para Magento 2
 * 
-* @category     Ã©lOOm
+* @category     elOOm
 * @package      Modulo PayUCl
 * @copyright    Copyright (c) 2021 Ã©lOOm (https://eloom.tech)
-* @version      1.0.1
+* @version      1.0.2
 * @license      https://opensource.org/licenses/OSL-3.0
 * @license      https://opensource.org/licenses/AFL-3.0
 *
